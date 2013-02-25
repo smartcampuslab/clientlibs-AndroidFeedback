53326795b0c0e15fc977356b0dfa3149c703ac21
giovanni.defrancesco <giovanni.defrancesco@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-02-25 14:34:54 +0000

    some build path changes

---
+++ gen/eu/trentorise/smartcampus/android/feedback/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package eu.trentorise.smartcampus.android.feedback;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
