78476ecaae3ed797edf179e8e83afbe7393b9137
giovanni.defrancesco <giovanni.defrancesco@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-02-26 17:09:47 +0000

    almost final version

--- src/eu/trentorise/smartcampus/android/feedback/fragment/SlidingFragment.java
+++ src/eu/trentorise/smartcampus/android/feedback/fragment/SlidingFragment.java
@@ -19,27 +19,25 @@ import eu.trentorise.smartcampus.android.feedback.utils.ScreenShooter;
 
 /**
  * This class helps to use the FeedbackFragment
+ * 
  * @author Giovanni De Francesco
- *
+ * 
  */
 
-public class SlidingFragment  extends Fragment{
-	
-	public static final  String APPID_PASSED_KEY="appid1";
-	public static final  String ACTID_PASSED_KEY="activityid1";
-	public static final  String IMG_PASSED_KEY="screenshotbitmap1";
-	
+public class SlidingFragment extends Fragment {
+
+	public static final String APPID_PASSED_KEY = "appid1";
+	public static final String ACTID_PASSED_KEY = "activityid1";
+	public static final String IMG_PASSED_KEY = "screenshotbitmap1";
+
 	private Bitmap mBitmap;
-	private String mAppId;
-	private String mActId;
 
 	private FragmentManager mFragmentManager;
 	private SlidingDrawer mSlidingDrawer;
 	private Button mSlidingButton;
-	private TextView mRateTextView;
 	private TextView mAssignementTextView;
-	
-	
+	private TextView mRateTextView;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstance) {
@@ -50,46 +48,58 @@ public class SlidingFragment  extends Fragment{
 	@Override
 	public void onStart() {
 		super.onStart();
-		mSlidingDrawer = (SlidingDrawer) getActivity()
-				.findViewById(R.id.feedback_sd);
-		mSlidingButton =(Button) getActivity().
-				findViewById(R.id.feedback_handle);
+		mSlidingDrawer = (SlidingDrawer) getActivity().findViewById(
+				R.id.feedback_sd);
+		mSlidingButton = (Button) getActivity().findViewById(
+				R.id.feedback_handle);
 		setSlidingDrawerListeners();
-		mRateTextView = (TextView) getActivity().findViewById(R.id.feedback_rate_link_tv);
-		mRateTextView.setMovementMethod(LinkMovementMethod.getInstance());
-		mAssignementTextView = (TextView)getActivity().findViewById(R.id.feedback_assignment_tv);
+		mRateTextView = (TextView) getActivity().findViewById(
+				R.id.feedback_rate_link_tv);
+		// mRateTextView.setMovementMethod(LinkMovementMethod.getInstance());
+		mAssignementTextView = (TextView) getActivity().findViewById(
+				R.id.feedback_assignment_tv);
+		// mAssignementTextView.setText("");
 	}
 
-
 	private void setSlidingDrawerListeners() {
-		mSlidingDrawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
-			
-			@Override
-			public void onDrawerOpened() {
-				
-				//This is an hack to avoid that the sliding drawer
-				//would be visible in the screenshot.
-				mSlidingDrawer.setVisibility(View.INVISIBLE);
-				mBitmap = ScreenShooter.viewToBitmap((getActivity().
-						findViewById(R.id.feedback_internal_root_rl)).getRootView());
-				mSlidingDrawer.setVisibility(View.VISIBLE);
-				
-				mSlidingButton.setBackgroundResource(R.drawable.btn_closefeedback);
-				((onDrawerVisibleListener)getActivity()).disableViewsBehindDrawer();
-				FeedbackFragment ff = (FeedbackFragment) getActivity().getSupportFragmentManager().
-						findFragmentById(R.id.feedback_content);
-				if(ff !=null)
-					ff.refreshImage(mBitmap);
-			}
-		});
-		mSlidingDrawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
-			
-			@Override
-			public void onDrawerClosed() {
-				mSlidingButton.setBackgroundResource(R.drawable.btn_openfeedback);
-				((onDrawerVisibleListener)getActivity()).enableViewsBehindDrawer();
-			}
-		});
+		mSlidingDrawer
+				.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
+
+					@Override
+					public void onDrawerOpened() {
+
+						// This is an hack to avoid that the sliding drawer
+						// would be visible in the screenshot.
+						mSlidingDrawer.setVisibility(View.INVISIBLE);
+						mBitmap = ScreenShooter.viewToBitmap((getActivity()
+								.findViewById(R.id.feedback_internal_root_rl))
+								.getRootView());
+						mSlidingDrawer.setVisibility(View.VISIBLE);
+
+						mSlidingButton
+								.setBackgroundResource(R.drawable.btn_closefeedback);
+
+						((onDrawerVisibleListener) getActivity())
+								.disableViewsBehindDrawer();
+						FeedbackFragment ff = (FeedbackFragment) getActivity()
+								.getSupportFragmentManager().findFragmentById(
+										R.id.feedback_content);
+						if (ff != null)
+							ff.refreshImage(mBitmap);
+
+					}
+				});
+		mSlidingDrawer
+				.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+
+					@Override
+					public void onDrawerClosed() {
+						mSlidingButton
+								.setBackgroundResource(R.drawable.btn_openfeedback);
+						((onDrawerVisibleListener) getActivity())
+								.enableViewsBehindDrawer();
+					}
+				});
 	}
 
 	/**
@@ -100,7 +110,6 @@ public class SlidingFragment  extends Fragment{
 		return mSlidingDrawer;
 	}
 
-
 	/**
 	 * Does a Fragment transaction replacing fragments
 	 * 
@@ -108,18 +117,26 @@ public class SlidingFragment  extends Fragment{
 	 *            the fragment that has to be shown
 	 * @param transactionTransition
 	 *            the transition cthat we want to apply
+	 * @param backstack
+	 *            if you want to add the fragent to the backstack
+	 * @param tag
+	 *            the tag of the fragment
 	 */
-	public void replaceFragmentWithTransition(FragmentManager fm,
-			Fragment fragment,Integer transactionTransition) {
-		if(mSlidingDrawer!=null&& mSlidingDrawer.isShown())
+	public void replaceFragmentWithTransition(Fragment fragment,
+			Integer transactionTransition, boolean backstack, String tag) {
+		if (mSlidingDrawer != null && mSlidingDrawer.isOpened())
 			mSlidingDrawer.animateClose();
-		FragmentTransaction fragmentTransaction = fm.beginTransaction();
-		if(transactionTransition==null)
+		FragmentTransaction fragmentTransaction = getActivity()
+				.getSupportFragmentManager().beginTransaction();
+		if (transactionTransition == null)
 			transactionTransition = FragmentTransaction.TRANSIT_FRAGMENT_OPEN;
-		mAssignementTextView.setText(getString(R.string.feedback_assignment,
-				fragment.getClass().getName()));
+		if (mAssignementTextView != null) {
+			mAssignementTextView.setText(getString(
+					R.string.feedback_assignment, tag));
+		}
+		fragmentTransaction.addToBackStack(fragment.getTag());
 		fragmentTransaction.setTransition(transactionTransition);
-		fragmentTransaction.replace(R.id.fragment_container, fragment);
+		fragmentTransaction.replace(R.id.fragment_container, fragment, tag);
 		fragmentTransaction.commit();
 	}
 
@@ -131,7 +148,8 @@ public class SlidingFragment  extends Fragment{
 		// Closing sliding drawer
 		if (mSlidingDrawer.isShown() && mSlidingDrawer.isOpened()) {
 			mSlidingDrawer.animateClose();
-		} else {
+			return true;
+		}/* else {
 			Fragment currentFragment = mFragmentManager
 					.findFragmentById(R.id.fragment_container);
 			// Checking if there is a fragment that it's listening for back
@@ -143,9 +161,8 @@ public class SlidingFragment  extends Fragment{
 				return managed;
 			}
 
-		}
+		}*/
 		return false;
 	}
-	
-	
+
 }
