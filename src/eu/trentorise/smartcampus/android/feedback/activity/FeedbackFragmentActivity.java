b20712332c2054f0afe541cc89d89e271abd6037
giovanni.defrancesco <giovanni.defrancesco@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-04-09 16:31:13 +0000

    Fixed marco's button bug.

--- src/eu/trentorise/smartcampus/android/feedback/activity/FeedbackFragmentActivity.java
+++ src/eu/trentorise/smartcampus/android/feedback/activity/FeedbackFragmentActivity.java
@@ -7,7 +7,9 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 import com.slidingmenu.lib.SlidingMenu;
@@ -18,30 +20,28 @@ import eu.trentorise.smartcampus.android.feedback.fragment.FeedbackSenderFragmen
 import eu.trentorise.smartcampus.android.feedback.utils.ScreenShooter;
 
 public abstract class FeedbackFragmentActivity extends SlidingFragmentActivity {
-	
+
 	protected SlidingMenu mSlidingMenu;
 	private boolean mUseFeedback;
-	
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// set the background fragent for the menu
+		// set the background fragment for the menu
 		setBehindContentView(R.layout.feedback_layout);
 		setUseFeedBack();
 		setUpMenu();
 	}
 
-
 	private void setUseFeedBack() {
 		try {
-			ApplicationInfo ai = getPackageManager().getApplicationInfo(this.getPackageName(),
-					PackageManager.GET_META_DATA);
-			Bundle aBundle=ai.metaData;
-			mUseFeedback=aBundle.getBoolean("use-feedback");
+			ApplicationInfo ai = getPackageManager().getApplicationInfo(
+					this.getPackageName(), PackageManager.GET_META_DATA);
+			Bundle aBundle = ai.metaData;
+			mUseFeedback = aBundle.getBoolean("use-feedback");
 		} catch (NameNotFoundException e) {
-			mUseFeedback=false;
+			mUseFeedback = false;
 			Log.e(FeedbackFragmentActivity.class.getName(),
 					"you should set the use-feedback metadata in app manifest");
 		}
@@ -60,21 +60,20 @@ public abstract class FeedbackFragmentActivity extends SlidingFragmentActivity {
 		mSlidingMenu.setMenu(R.layout.feedback_fragment_layout);
 		mSlidingMenu.setMode(SlidingMenu.RIGHT);
 		mSlidingMenu.setBehindOffsetRes(R.dimen.feedback_fragment_offset);
-		
-		if(!mUseFeedback)
+
+		if (!mUseFeedback)
 			mSlidingMenu.setSlidingEnabled(false);
-		
+
 		setUpMenuListeners();
 	}
-	
+
 	public boolean isUsingFeedback() {
 		return mUseFeedback;
 	}
 
-
 	@Override
 	public void toggle() {
-		if(mUseFeedback)
+		if (mUseFeedback)
 			super.toggle();
 	}
 
@@ -83,15 +82,23 @@ public abstract class FeedbackFragmentActivity extends SlidingFragmentActivity {
 
 			@Override
 			public void onOpened() {
-				//This two lines are necessary for 2.2 compatibility
+				// This two lines are necessary for 2.2 compatibility
 				View v = findViewById(android.R.id.content);
 				v.invalidate();
-				
+
 				refreshFragment();
+				toggleHandleButton();
+			}
+		});
+		mSlidingMenu.setOnClosedListener(new SlidingMenu.OnClosedListener() {
+
+			@Override
+			public void onClosed() {
+				toggleHandleButton();
 			}
 		});
 	}
-	
+
 	protected void refreshFragment() {
 		FeedbackSenderFragment ff = (FeedbackSenderFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.feedback_fragment_container);
@@ -100,18 +107,29 @@ public abstract class FeedbackFragmentActivity extends SlidingFragmentActivity {
 			Bitmap bmp = ScreenShooter.viewToBitmap(mSlidingMenu.getContent());
 			ff.refreshImage(bmp.copy(Bitmap.Config.RGB_565, false));
 			if (getSupportActionBar().getSelectedTab() != null) {
-				ff.refreshText(getSupportActionBar().getSelectedTab().getText().toString());
+				ff.refreshText(getSupportActionBar().getSelectedTab().getText()
+						.toString());
 			} else if (getSupportActionBar().getTitle() != null) {
 				ff.refreshText(getSupportActionBar().getTitle().toString());
 			}
-			Fragment f = getSupportFragmentManager().findFragmentById(android.R.id.content);
+			Fragment f = getSupportFragmentManager().findFragmentById(
+					android.R.id.content);
 			if (f != null) {
 				ff.refreshActivity(f.getClass().getName());
 			}
 		}
 	}
-	
+
+	public void toggleHandleButton() {
+		Button handleButton = (Button) findViewById(R.id.feedback_button_handler);
+		if (getSlidingMenu().isMenuShowing())
+			handleButton.setBackgroundResource(R.drawable.btn_closefeedback);
+		else
+			handleButton.setBackgroundResource(R.drawable.btn_openfeedback);
+	}
+
 	public abstract String getAppToken();
+
 	public abstract String getAuthToken();
 
 }
